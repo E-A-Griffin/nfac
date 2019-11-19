9bfbf582d793429d10840e94519432d9880948fb
E-A-Griffin <53583563+E-A-Griffin@users.noreply.github.com>
2019-11-18 22:47:30 -0500

    Delete objects.clj

--- src/dfac/objects.clj
+++
@@ -1,9 +0,0 @@
-(ns dfac.objects)
-
-;; Atom to hold map of states shared by other files in dfac
-(def objects (atom {}))
-
-;; Atom to hold test strings to be evaluated for membership
-;; in a given DFA.
-(def test-strings (atom []))
-
